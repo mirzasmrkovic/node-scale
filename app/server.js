56983cbd0a1bdb9313ec37d5f3f04cd140e1347e
Mirza Smrkovic <mike@Mirzas-MacBook-Pro.local>
2019-07-23 03:14:16 +0200

    refactor server.js, add app/favicon.ico

--- app/server.js
+++ app/server.js
@@ -1,31 +1,21 @@
 const fs = require('fs')
 const http = require('http')
 const port = 3000
-// const styles = require('styles.css')
 
-let writeToHeader = (response, data) => {
-  response.write(data)
-  response.end()
+let writeToHead = (url, response) => {
+  let extension = url.substring(url.lastIndexOf('.') + 1)
+  let isInsideRoot = url.indexOf('/') < 0 ? true : false
+
+  response.writeHead(200, {'Content-Type': `text/${extension}` })
+  fs.readFile(`${(!isInsideRoot ? '.' : '') + url}`, null, (err, data) => {
+    if(err) throw err
+    response.write(data)
+    response.end()
+  })
 }
 
 const server = http.createServer((request, response) => {
-  switch (request.url) {
-    case '/css/styles.css' :
-      response.writeHead(200, {'Content-Type': 'text/css'})
-      fs.readFile('css/styles.css', null, (err, data) => writeToHeader(response, data))
-      break
-    case '/' :
-      response.writeHead(200, {'Content-Type': 'text/html'})
-      fs.readFile('index.html', null, (err, data) => writeToHeader(response, data))
-      break
-    case '/app.js' :
-      response.writeHead(200, {'Content-Type': 'text/js'})
-      fs.readFile('app.js', null, (err, data) => writeToHeader(response, data))
-      break
-    case '/favicon.ico' :
-      response.writeHead(200, {'Content-Type': 'text/icon'})
-      fs.readFile('app.js', null, (err, data) => writeToHeader(response, data))
-    }
-
-
+  let url = request.url
+  if(url !== '/') writeToHead(url, response)
+  else writeToHead('index.html', response)
 }).listen(port)
