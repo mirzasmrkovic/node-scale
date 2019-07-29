41bbc8ddd126bea07382353e1ea29e1947d680bd
Mirza Smrkovic <mirza.smrkovic@gmail.com>
2019-07-29 14:08:37 +0200

    add express

--- app/server.js
+++ app/server.js
@@ -1,21 +1,11 @@
 const fs = require('fs')
-const http = require('http')
+const express = require('express')
+const app = express()
+const path = require('path')
+const router = express.Router()
 const port = 3000
 
-let writeToHead = (url, response) => {
-  let extension = url.substring(url.lastIndexOf('.') + 1)
-  let isInsideRoot = url.indexOf('/') < 0 ? true : false
+router.get('/', (req,res) => { res.sendFile(path.join(__dirname+'/index.html')) })
 
-  response.writeHead(200, {'Content-Type': `text/${extension}` })
-  fs.readFile(`${(!isInsideRoot ? '.' : '') + url}`, null, (err, data) => {
-    if(err) throw err
-    response.write(data)
-    response.end()
-  })
-}
-
-const server = http.createServer((request, response) => {
-  let url = request.url
-  if(url !== '/') writeToHead(url, response)
-  else writeToHead('index.html', response)
-}).listen(port)
+app.use('/', router)
+app.listen(process.env.port || port)
